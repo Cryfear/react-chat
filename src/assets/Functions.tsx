5ca0463f09b1afdf07edb90fbedd90eb8b004069
Cryfear <vasil.arturok@yandex.ru>
2021-02-12 05:32:56 +0300

    must to redo it

--- src/assets/Functions.tsx
+++
@@ -1,8 +0,0 @@
-import { SearchitemTypes } from "../components/Home/HomeTypes";
-
-export const SearchFunction = (item: SearchitemTypes, searchValue: string) => {
-  // поиск по инпуту
-  if (searchValue === "") return true;
-  if (item.fullName.indexOf(searchValue) > -1) return true;
-  return false;
-};
